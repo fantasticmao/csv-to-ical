164e74bf5a9ec694767af7eadecc5d3f682adfc9
fantasticmao <maomao8017@gmail.com>
2025-01-03 22:47:34 +0800

    feat: update unit tests

--- common/config_test.go
+++ common/config_test.go
@@ -17,9 +17,11 @@ func TestParseConfig(t *testing.T) {
 
 	assert.Equal(t, "testdata/calendar_test.csv", config.CsvProviders["foo"].File)
 	assert.Equal(t, "zh_cn", config.CsvProviders["foo"].Language)
-	assert.Equal(t, 3, config.CsvProviders["foo"].RecurCnt)
+	assert.Equal(t, 2, config.CsvProviders["foo"].RecurCnt)
+	assert.Equal(t, 0, config.CsvProviders["foo"].BackCnt)
 
 	assert.Equal(t, "https://raw.githubusercontent.com/fantasticmao/csv-to-ical/main/csv/testdata/calendar_test.csv", config.CsvProviders["bar"].Url)
 	assert.Equal(t, "en", config.CsvProviders["bar"].Language)
-	assert.Equal(t, 5, config.CsvProviders["bar"].RecurCnt)
+	assert.Equal(t, 3, config.CsvProviders["bar"].RecurCnt)
+	assert.Equal(t, 1, config.CsvProviders["bar"].BackCnt)
 }
